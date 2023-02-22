fbf7899ec77dea394645e67db279294f58666f3f
sampath <sampathsurukutla12@gmail.com>
2023-02-22 08:43:58 +0000

    Tests for Get Date After X Days

---
+++ index.js
@@ -0,0 +1,8 @@
+const addDays = require("date-fns/addDays");
+const result = (days) => {
+  const newDate = addDays(new Date(2020, 7, 22), days);
+  return `${newDate.getDate()}-${
+    newDate.getMonth() + 1
+  }-${newDate.getFullYear()}`;
+};
+module.exports = result;
